45b9cec2ef888804f82bef374527d3fd7b5f628e
Marina Portugal <marinapsvreis@gmail.com>
2023-04-21 11:07:07 -0300

    :lipstick: Componentizando avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post(props) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/marinapsvreis.png"
-          />
+          <Avatar src="https://github.com/marinapsvreis.png"/>
           <div className={styles.authorInfo}>
             <strong>{props.author}</strong>
             <span>{props.role}</span>
